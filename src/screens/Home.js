038eebd2183ab539287c3e86def5d62f3d97387e
angieamorena <mamorena@udesa.edu.ar>
2022-11-08 22:45:08 -0300

    hice el home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,41 +1,101 @@
 import React, { Component } from 'react';
-import {View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native-web';
-import {db, auth} from '../../firebase/config'; 
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-web';
+import { db, auth } from '../../firebase/config';
+import firebase from 'firebase';
 
-
-class Home extends Component{
-    constructor(props) {
+class Home extends Component {
+	constructor(props) {
 		super(props);
 		this.state = {
-            users: []
-        }
-    }
+			users: [],
+			posteos: [],
+			descripcion: '',
+            like: false
+		};
+	}
+	
+	componentDidMount() {
+	
 
-componentDidMount (){
-    db.collection ('users').onSnapshot ((docs) => {
-      let usersFromDb = []
-      docs.forEach ((doc)=> {
-        let user = doc.data();
-        usersFromDb.push ({id: doc.id , data: user});
-      });
-      this.setState ({users: usersFromDb}); 
-    });
+		db.collection('posts').orderBy('createdAt', 'desc').onSnapshot((docs) => {
+				let postsFromDb = [];
+				docs.forEach((doc) => {
+					let post = doc.data();
+					postsFromDb.push({ id: doc.id, data: post });
+				});
+				console.log(postsFromDb);
+
+				this.setState({ posteos: postsFromDb });
+			});
+	}
+
+
+	likear(idDelPosteo) {
+		db.collection('posts').doc(idDelPosteo).update({
+				likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email),
+			})
+			.then(res=> {this.setState({
+                like: true
+            });})
+			.catch((err) => console.log(err));
+	}
+
+    sacarLike(idDelPosteo){
+        db.collection('posts').doc(idDelPosteo).update({
+        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+        })
+        .then(res=> {this.setState({
+            like: false
+        });})
+        .catch ((err)=> console.log(err))
+        }
+        
+	render() {
+		return (
+			<>
+				<Text style= {styles.titulo} > Dream Team </Text>
+				
+                <FlatList 
+					data={this.state.posteos}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => (
+						<View style= {styles.container}> 
+                            <Text style= {styles.titulo2} >{item.data.owner}</Text>
+							<Text style= {styles.text} > DESCRIPCION: {item.data.description}</Text>
+                            { this.state.like == false ? ( 
+							<TouchableOpacity onPress={() => this.likear(item.id)}>
+								<Text style= {styles.button} >Dar Like</Text>
+							</TouchableOpacity>
+                            ) : (<TouchableOpacity onPress={()=> this.sacarLike(item.id)}>
+                            <Text> Sacar Like </Text> 
+                            </TouchableOpacity>
+                            
+                            )} 
+						</View>
+					)}
+				/>
+             
+			</>
+		);
+	}
 }
 
-    render (){
-        return(
-        <View>
-        <Text>Posteos</Text>
-        
-        </View>
-        )
-        
-    }
-};
-    
-
 const styles = StyleSheet.create({
+    titulo:{
+        textAlign: 'center',
+        fontWeight: 50,
+        fontSize: 100,
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    titulo2:{
+        textAlign: 'center',
+        fontWeight: 50,
+        fontSize: 50,
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
     container:{
         flex:1,
         display: 'flex',
@@ -43,10 +103,23 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderWidth:1,
         borderRadius:5,
-        backgroundColor:'black',
         paddingVertical:16,
         paddingHorizontal:8,
-    }
+    }, 
+    button:{
+        fontSize: 18,
+        color: 'red',
+   },
+    text:{
+        textAlign: 'center',
+        fontWeight: 50,
+        fontSize: 15,
+        paddingTop: 10,
+        paddingBottom: 10,
+   }
 })
 
 export default Home;
+
+    
+
